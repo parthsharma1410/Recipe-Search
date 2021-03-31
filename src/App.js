5148f3125a1c7c6ef36ab666af6b5af1aab05d64
parthsharma1410 <parth.sharma2019a@vitstudent.ac.in>
2021-04-01 00:50:25 +0530

    Added API and made responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import MealList from "./components/ResultList"
 
 function App() {
+  const [mealData, setMealData] = useState(null)
+  const [calories, setCalories] = useState(2000)
+
+  function getMealData() {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=cb1c464d94f142c08b156c5beddade8b&timeFrame=day&targetCalories=${calories}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setMealData(data)
+      })
+      .catch(() => {
+        console.log("error")
+      })
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="controls">
+        <div className='para'>
+      <h2 className="heading animate__animated animate__fadeInUp">GET RECIPES BASED ON CALORIES ENTERED!</h2>
+      <br />
+      <br />
+      <p className="animate__animated animate__fadeInUp animate__delay-1s">
+        Now get direct recipes, just on one click, based on the number of calories you want in your meal. <br />
+        This is made possible incorporating the spoonacular API, giving <br />
+        the entire info on the recipes and the macronutrients in the meal.
+      </p>
+        </div>
+      <div className="form animate__animated animate__fadeInUp animate__delay-2s">
+        <input
+          type="number"
+          placeholder="Enter calories in numbers"
+          onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button> 
+      </div>
+      </section>
+      {mealData && <MealList mealData={mealData} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
